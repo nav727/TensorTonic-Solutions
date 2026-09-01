87da8c0039251b1c794c5aea5ed1a28a0b42b6d4
Navneet Mann <navneetcsp@gmail.com>
2026-09-01 21:42:31 +0100

    Solve mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -12,6 +12,8 @@ def mean_median_mode(x: list) -> dict:
     distinct_nums, counts = np.unique(x, return_counts=True)
     mode = distinct_nums[np.argmax(counts)]
     
-    return {'mean'  : float(np.mean(x)),
-           'median' : float(np.median(x)),
-           'mode'   : float(mode)}
+    return {
+        'mean'  : float(np.mean(x)),
+        'median' : float(np.median(x)),
+        'mode'   : float(mode)
+           }
