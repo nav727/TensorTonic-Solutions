8a214a2c40ad8c8b046f8016327e23488798a816
Navneet Mann <navneetcsp@gmail.com>
2026-09-01 22:40:27 +0100

    Solve softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -7,9 +7,12 @@ def softmax(x: list) -> np.ndarray:
     """
     
     x = np.array(x)
-    
+
+    # when input 2D, then all operations are row wise
     axis = 1 if (x.ndim > 1) else 0
-    
+
+    # subtract max for overflow issue
     x = x - np.max(x, axis=axis, keepdims=True)
+    
     return np.exp(x) / np.sum(np.exp(x), axis=axis, keepdims=True)
   
