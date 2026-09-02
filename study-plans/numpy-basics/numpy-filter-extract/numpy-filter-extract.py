b284a53926576ad6e0915ae06f31168a82766550
Navneet Mann <navneetcsp@gmail.com>
2026-09-02 23:36:03 +0100

    Solve numpy-filter-extract

--- study-plans/numpy-basics/numpy-filter-extract/numpy-filter-extract.py
+++ study-plans/numpy-basics/numpy-filter-extract/numpy-filter-extract.py
@@ -11,5 +11,7 @@ def filter_and_extract(data, row_start, row_stop, threshold):
     data = np.matrix.flatten(data)
     
     # mask will remove the elements from 1D array
+    # use where in case you want to replace false places by zero
+    # np.where(mask, a, 0))
     mask = (data > threshold)
     return data[mask]
