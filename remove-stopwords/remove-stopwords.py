0338c7cbfcb491befd5c2aa8643049dc5c38242c
Navneet Mann <navneetcsp@gmail.com>
2026-09-04 21:21:40 +0100

    Solve remove-stopwords

--- remove-stopwords/remove-stopwords.py
+++ remove-stopwords/remove-stopwords.py
@@ -2,7 +2,8 @@ def remove_stopwords(tokens: list, stopwords: list) -> list:
     """
     Returns a list of tokens.
     """
-    
+
+    # set membership is cheaper to compare
     stopwords = set(stopwords)
     ans = []
     for token in tokens:
