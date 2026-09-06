e30168ceca55acfd984bfc25027c6179c7cf96ba
Navneet Mann <navneetcsp@gmail.com>
2026-09-06 20:18:58 +0100

    Solve numpy-angle-features

---
+++ study-plans/numpy-basics/numpy-angle-features/numpy-angle-features.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def angle_features(angles):
+    """Returns: np.ndarray of shape (3, n), rows are sin, cos, tan"""
+    
+    angles = np.array(angles)
+
+    # angles should be in radians
+    sin_angles = np.sin(angles)
+    cos_angles = np.cos(angles)
+    tan_angles = np.tan(angles)
+
+    combined = np.stack([sin_angles, cos_angles, tan_angles], dtype = 'float64')
+    
+    return combined
