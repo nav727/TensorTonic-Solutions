cca58a3e91fb3c3cbac987669f9b9059962d681b
Navneet Mann <navneetcsp@gmail.com>
2026-09-06 21:29:06 +0100

    Solve numpy-row-extremes

--- study-plans/numpy-basics/numpy-row-extremes/numpy-row-extremes.py
+++ study-plans/numpy-basics/numpy-row-extremes/numpy-row-extremes.py
@@ -7,9 +7,9 @@ def row_extremes(data):
     data = np.array(data, dtype='float64')
     
     max_val = np.max(data, axis = 1)
-    max_col = np.argmax(data, axis=1)
+    max_col = np.argmax(data, axis = 1)
     min_val = np.min(data, axis = 1)
-    min_col = np.argmin(data, axis=1)
+    min_col = np.argmin(data, axis = 1)
 
     combined = np.stack([max_val, max_col, min_val, min_col])
 
