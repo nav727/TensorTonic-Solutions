e1241c483153eab29e12c80bf7e39ff491c2cbfd
Navneet Mann <navneetcsp@gmail.com>
2026-09-23 19:37:30 +0100

    Solve nlp-tokenization

--- study-plans/cracking-nlp/nlp-tokenization/nlp-tokenization.py
+++ study-plans/cracking-nlp/nlp-tokenization/nlp-tokenization.py
@@ -3,33 +3,38 @@ def tokenize(text: str) -> list:
     Returns a list of token strings.
     """
 
-    
+    text = text.strip()
     ans = []
     word = ''
 
-    text = text.strip()
     for ele in text:
         
         # check alphanumeric or underscore
-        # continue the token
         if ele.isalnum() or ele == '_':
             word += ele
-    
+
+        # preserve separate token
+        # discard for whitespace
         else: 
             
             # discard if space
+            # checking using --> ele != ' ' is the wrong way!
             if ele.isspace():
+                
                 if word != '':
                     ans.append(word)
+            
                 word = ''
     
             # add punc separately
             else:
                 if word != '':
                     ans.append(word)
+                
                 word = ''
                 ans.append(ele)
-    
+
+    # append the last token    
     if word != '':
         ans.append(word)
         
